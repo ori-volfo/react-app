2100dd60fe4a1d4f4b2c3e8638c589a1f68669c3
ori-volfo <ori.volfo@gmail.com>
2017-11-27 08:38:53 +0200

    props vs. state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React, { Component } from 'react';
 
-const App =() => <h1>Hi!</h1>;
+class App extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            txt: 'this is the state text'
+        }
+    }
+    update(e){
+        this.setState({txt: e.target.value})
+    }
+    render(){
+        return (
+            <div>
+                <input type="text" onChange={this.update.bind(this)}/>
+                <h1>{this.state.txt}</h1>
+                <h2>{this.props.txt}</h2>
+            </div>
+        )
+    }
+}
 
 export default App;
